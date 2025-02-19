2accd91a9b52804d5ea4707eaa9ebb1773da3678
Semih Esentürk <semihesenturk@outlook.com>
2025-02-19 15:05:20 +0300

    InMemoryCache operations has added
    InMemoryCache operations has added

---
+++ Web/SE.RedisInMemory/InmemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InmemoryApp.Web.Controllers;
+
+public class ProductController(IMemoryCache memoryCache) : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        if (!memoryCache.TryGetValue("time", out string timeCache))
+        {
+            var options = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(1),
+                SlidingExpiration = TimeSpan.FromSeconds(10),
+                Priority = CacheItemPriority.High
+            };
+            
+            memoryCache.Set("time", DateTime.Now.ToString(), options);
+        }
+        
+        return View();
+    }
+
+    public IActionResult Show()
+    {
+        memoryCache.TryGetValue("time", out string timeCache);
+        
+        ViewBag.Zaman = timeCache;
+        return View();
+    }
+}
--- Web/SE.RedisInMemory/InmemoryApp.Web/Program.cs
+++ Web/SE.RedisInMemory/InmemoryApp.Web/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Add MemoryCache
+builder.Services.AddMemoryCache();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
