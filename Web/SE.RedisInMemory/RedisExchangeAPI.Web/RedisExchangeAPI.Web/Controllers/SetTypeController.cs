546b1e2027709abdad44f213626c60748361f135
Semih Esentürk <semihesenturk@outlook.com>
2025-02-21 23:56:03 +0300

    SetType operations has added
    SetType operations has added

---
+++ Web/SE.RedisInMemory/RedisExchangeAPI.Web/RedisExchangeAPI.Web/Controllers/SetTypeController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers;
+
+public class SetTypeController(RedisService redisService) : Controller
+{
+    private readonly IDatabase _db = redisService.GetDatabase(2);
+    private string _listKey = "hashNames";
+
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        HashSet<string> hashNames = [];
+
+        if (_db.KeyExists(_listKey))
+        {
+            (await _db.SetMembersAsync(_listKey)).ToList().ForEach(s=>
+                hashNames.Add(s.ToString())); ;
+        }
+        
+        return View(hashNames);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(string name)
+    {
+        _db.KeyExpire(_listKey, TimeSpan.FromMinutes(5));
+        await _db.SetAddAsync(_listKey, name);
+        
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteItem(string name)
+    {
+        await _db.SetRemoveAsync(_listKey, name);
+        return RedirectToAction(nameof(Index));
+    }
+}
