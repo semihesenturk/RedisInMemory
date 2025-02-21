849d1f68cafd95dfc7e6f642b505eb11b83a8568
Semih Esentürk <semihesenturk@outlook.com>
2025-02-21 11:03:00 +0300

    Simple redis cache operations
    Simple redis cache operations

--- Web/SE.RedisInMemory/IDistributedCacheRedisApp/IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ Web/SE.RedisInMemory/IDistributedCacheRedisApp/IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -5,11 +5,27 @@ namespace IDistributedCacheRedisApp.Web.Controllers;
 
 public class ProductsController(IDistributedCache distributedCache) : Controller
 {
-    private readonly IDistributedCache _distributedCache = distributedCache;
-
     // GET
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var cacheOptions = new DistributedCacheEntryOptions();
+        cacheOptions.SetSlidingExpiration(TimeSpan.FromSeconds(30));
+        cacheOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+
+        await distributedCache.SetStringAsync("time", DateTime.Now.ToString(), cacheOptions);
+        return View();
+    }
+
+    public async Task<IActionResult> Show()
+    {
+        var getTime = await distributedCache.GetStringAsync("time");
+        ViewBag.Time = getTime;
+        return View();
+    }
+
+    public async Task<IActionResult> Remove()
+    {
+        await distributedCache.RemoveAsync("time");
         return View();
     }
 }
